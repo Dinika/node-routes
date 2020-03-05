41df824d202928fb1e9632628bfeb8b8a060beea
Dinika <dinikasaxenas@gmail.com>
2020-03-05 19:02:31 +0100

    Pass request through two middlewares
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(function (req, res, next) {
+  console.log('First middleware');
+  next();
+});
+
+app.use(function (req, res, next) {
+  console.log('Second middleware');
+  res.send('<p>Is this the real life?</p>');
+});
+
 app.listen(8080);
